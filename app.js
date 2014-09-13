2aef873b9868aee9aa75e5b1e078ac214167bf6e
cmserna <christopherserna@christophersmbp.gateway.2wire.net>
2014-09-12 20:21:18 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var clip = new ZeroClipboard( document.getElementById("copy-button"), {
+  moviePath: "zeroclipboard.swf"
+} );
+clip.on('mousedown',function() {
+  clip.setText(document.getElementById('copy-content').value);
+  console.log("copying...");
+});
+clip.on('complete',function(client,args) {
+  console.log('copied: ' + args.text);
+});
