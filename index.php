8425cd25eeb22a1e1d2ec16eee2e3ac87a268326
cmserna <christopherserna@christophersmbp.gateway.2wire.net>
2014-09-12 21:39:29 -0700

    added homepage

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include("head.php"); ?>
+
+  <?php include("navbar.php"); ?>
+
+    <div class="container">
+     <div class="row">
+        <div class="col-md-5">
+          <div class="panel panel-primary">
+            <div class="panel-heading">Templates</div>
+            <div class="panel-body">
+              <div class="list-group">
+                <a href="raw.php" class="list-group-item">Raw</a>
+                <a href="graded.php" class="list-group-item">Graded</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php include("footer.php"); ?>
