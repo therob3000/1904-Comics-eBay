7d138ce98c95a4f00c9f361656dc0a170f3fae5e
cmserna <christopherserna@christophersmbp.gateway.2wire.net>
2014-09-12 21:39:00 -0700

    added homepage

--- navbar.php
+++ navbar.php
@@ -14,7 +14,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="index.php">Raw</a></li>
+        <li><a href="raw.php">Raw</a></li>
         <li><a href="graded.php">Graded</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
